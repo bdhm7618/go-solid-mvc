2719dd08d348d33ddf8a75cb8f0a851e882eecf6
Envaglo PC <fox.arb.2017@gmail.com>
2026-01-18 14:47:09 +0200

    save

--- main.go
+++ main.go
@@ -4,4 +4,24 @@ import "fmt"
 
 func main() {
 	fmt.Print("hello MVC Framework") ; 
+
+
+	var data map[string]any = map[string]any{
+		"name" : "Abdo Mostafa" , 
+		"email":"abdo@gmail.com" , 
+		"phone" : "010953285567",
+		"address" : [] any{
+			"address one" , 
+			"address two" , 
+			12321 , 
+		} ,
+	}
+
+	rules := map[string]any {
+        "name": "required|min:3" , 
+		"email": "required" , 
+	}
+	
+	validate(data ,  rules) ; 
+	fmt.Print(data , rules) ;
 }
